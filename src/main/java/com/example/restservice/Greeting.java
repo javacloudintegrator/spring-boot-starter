1fd9d5abfe5d6e95ca03732a4bf65cdf2de9fd80
timkitch <timkitch@gmail.com>
2021-03-16 08:53:42 -0400

    Initial commit

---
+++ src/main/java/com/example/restservice/Greeting.java
@@ -0,0 +1,26 @@
+package com.example.restservice;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+public class Greeting {
+
+  @Getter
+	private final long id;
+
+  @Getter
+  @Setter
+	private final String content;
+
+  @Getter
+  @Setter
+  private List<String> list = new ArrayList<>();
+
+	public Greeting(long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+
+}
---
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -0,0 +1,33 @@
+package com.example.restservice;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+
+@RestController
+public class GreetingController {
+
+  private static final String template = "Hello, %s!";
+  private final AtomicLong counter = new AtomicLong();
+
+  @GetMapping("/")
+  public String defaultMessage() {
+    return "Welcome to the Spring Boot Application Starter!";
+  }
+
+  @GetMapping("/greeting")
+  public ResponseEntity<Greeting> greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    return new ResponseEntity<>(new Greeting(counter.incrementAndGet(), String.format(template, name)),HttpStatus.OK);
+  }
+
+  @PostMapping("/greeting")
+  public ResponseEntity<Greeting> greeting(@RequestBody Person person) {
+    return new ResponseEntity<>(new Greeting(counter.getAndIncrement(), String.format(template, person.getName())), HttpStatus.OK);
+  }
+}
